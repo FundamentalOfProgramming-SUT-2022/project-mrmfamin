a586a5f141782ffe026fa7d2aa4f68f70d9889be
mfamin <mrmfamin835@yahoo.com>
2023-01-20 19:27:47 +0330

    first insert str commands

--- project.c
+++ project.c
@@ -4,10 +4,17 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#define MAX_LINE 2000
-#define MAX_PATH_ADDRESS 2000
+#define MAX_LINE 5000
+#define MAX_PATH_ADDRESS 500
+#define MAX_STRING 2000
 
-char * createfile(char * line){
+int file_exist(char * path);
+char * createfile(char * line);
+char * get_path(char * line);
+
+
+
+void createfile(char ** line){
     //end types
     const char end_str_with[] = {' ', '\"'};
 
@@ -15,41 +22,105 @@ char * createfile(char * line){
     int ending_type = 0;
 
     //Checking end type
-    if(*(line + 7) == '\"'){
+    if(*(*(line) + 7) == '\"'){
         ending_type = 1;
-        line++;
+        *(line)++;
     }
 
     //Go to firs of addres
-    line+=8;
+    *(line)+=8;
 
     //Analyze path and create folders
     char * path_to_make = (char *)malloc((MAX_PATH_ADDRESS + 1) * sizeof(char));
     int i = 0;
-    while(*line != end_str_with[ending_type] && *line != '\0'){
-        if(*line == '/'){
+    while(**line != end_str_with[ending_type] && **line != '\0'){
+        if(**line == '/'){
             *(path_to_make + i) = '\0';
             mkdir(path_to_make);
         }
-        *(path_to_make + i) = *line;
+        *(path_to_make + i) = **line;
         i++;
-        line++;
+        *(line)++;
     }
     *(path_to_make + i) = '\0';
 
     //create file if not exist
-    FILE *file_to_make = fopen(path_to_make, "r");
-    if(file_to_make) {
-        puts("File is exist");
+    FILE *file_to_make;
+    if(file_exist(path_to_make)) {
+        puts("File already exist");
     } else {
         file_to_make = fopen(path_to_make, "w");
     }
     fclose(file_to_make);
 
-    line += ending_type;
+    *(line) += ending_type;
+}
 
+int file_exist(char * path){
+    FILE *check = fopen(path, "r");
+    if(check) {
+        return 1;
+    }else{
+        return 0;
+    }
+}
 
-    return line;
+char * get_path(char ** line){
+    //end types
+    const char end_str_with[] = {' ', '\"'};
+
+    //char * path = line;
+    int ending_type = 0;
+
+    //Checking end type
+    if(*(*(line) + 7) == '\"'){
+        ending_type = 1;
+        *(line)++;
+    }
+
+    //Go to firs of addres
+    *(line)+=8;
+
+    //Analyze path and create folders
+    char * path = (char *)malloc((MAX_PATH_ADDRESS + 1) * sizeof(char));
+    int i = 0;
+    while(**line != end_str_with[ending_type] && **line != '\0'){
+        *(path + i) = **line;
+        i++;
+        *(line)++;
+    }
+    *(path + i) = '\0';
+
+    return path;
+}\
+
+char * get_string(char ** line){
+    //end types
+    const char end_str_with[] = {' ', '\"'};
+
+    //char * path = line;
+    int ending_type = 0;
+
+    //Checking end type
+    if(*(*(line) + 6) == '\"'){
+        ending_type = 1;
+        *(line)++;
+    }
+
+    //Go to firs of addres
+    *(line)+=6;
+
+    //Analyze path and create folders
+    char * path = (char *)malloc((MAX_STRING + 1) * sizeof(char));
+    int i = 0;
+    while(**line != end_str_with[ending_type] && **line != '\0'){
+        *(path + i) = **line;
+        i++;
+        *(line)++;
+    }
+    *(path + i) = '\0';
+
+    return path;
 }
 
 int main()
@@ -59,13 +130,27 @@ int main()
 
     while (-1)
     {
+        //get line
         char * notAnalyzed = line;
         fgets(line, MAX_LINE, stdin);
-        line[strlen(line) - 1] = '\0'; //replace \n
+
+        //replace \n
+        line[strlen(line) - 1] = '\0'
+
+        //explode line to command
         sscanf(notAnalyzed, "%s", command);
         notAnalyzed += strlen(command) + 1;
+
+        //command switch
         if(strcmp(command, "createfile") == 0){
-            notAnalyzed = createfile(notAnalyzed);
+            createfile(&notAnalyzed);
+        }else if(strcmp(command, "insertstr") == 0){
+            if(file_exist(get_path(&notAnalyzed))){
+                char * string = get_string(&notAnalyzed);
+                printf("%s\n", string);
+            }else{
+                puts("File isn't exist");
+            }
         }
     }
 
